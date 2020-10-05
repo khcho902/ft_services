9b68ba19ef4f6e8fa3404ce7e86659a39c602ac2
khcho902 <khcho902@gmail.com>
2020-10-06 01:49:38 +0900

    done ftps

---
+++ srcs/ftps/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+			-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" \
+			-keyout /etc/ssl/private/vsftpd.key \
+			-out /etc/ssl/certs/vsftpd.crt
+
+mkdir -p /var/ftp
+adduser -D -h /var/ftp ${FTP_USERNAME}
+echo "${FTP_USERNAME}:${FTP_PASSWORD}" | chpasswd
+
+vsftpd /etc/vsftpd/vsftpd.conf
