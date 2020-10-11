c4b692dc6dd39529710407e3edcd81cbfebb3345
khcho902 <khcho902@gmail.com>
2020-10-11 17:04:33 +0900

    done wordpress

--- setup.sh
+++ setup.sh
@@ -17,9 +17,11 @@ docker build -t nginx-image ./srcs/nginx
 docker build -t ftps-image ./srcs/ftps
 docker build -t mysql-image ./srcs/mysql
 docker build -t phpmyadmin-image ./srcs/phpmyadmin
+docker build -t wordpress-image ./srcs/wordpress
 
 #######################################
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
