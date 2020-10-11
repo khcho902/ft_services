5f5fd2bea2cb40c2a80d13368f6d45caab84448a
khcho902 <khcho902@gmail.com>
2020-10-11 13:25:44 +0900

    doing...

--- setup.sh
+++ setup.sh
@@ -16,10 +16,10 @@ kubectl apply -f ./srcs/metallb/metallb.yaml
 docker build -t nginx-image ./srcs/nginx
 docker build -t ftps-image ./srcs/ftps
 docker build -t mysql-image ./srcs/mysql
+docker build -t phpmyadmin-image ./srcs/phpmyadmin
 
 #######################################
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
-
-
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
