667b5931b0876b37378f1173ddd32f60621e6ecd
khcho902 <khcho902@gmail.com>
2020-10-19 11:24:20 +0900

    done shared ip

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -37,8 +37,8 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-define ('WP_HOME', 'http://192.168.99.103:5050');
-define ('WP_SITEURL', 'http://192.168.99.103:5050');
+define ('WP_HOME', 'http://192.168.99.100:5050');
+define ('WP_SITEURL', 'http://192.168.99.100:5050');
 
 /**#@+
  * Authentication Unique Keys and Salts.
