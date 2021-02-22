feee717cba3dd6bde7aeecbbcf3028c8708dda8d
khcho902 <khcho902@gmail.com>
2021-02-23 03:24:15 +0900

    input several users and an administrator in wordpress db

--- srcs/wordpress/srcs/wp-config.php
+++ srcs/wordpress/srcs/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'admin' );
+define( 'DB_PASSWORD', 'admin1234' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql' );
