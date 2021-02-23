133c42320a414c3e140b4c859933fe77af82aea6
khcho902 <khcho902@gmail.com>
2021-02-24 01:40:52 +0900

    remove hard-coding(ip address) in ftps.yaml

--- setup.sh
+++ setup.sh
@@ -18,6 +18,8 @@ docker build -t wordpress-image ./srcs/wordpress
 # Apply yaml files
 kubectl apply -f ./srcs/metallb/metallb.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+export FTPS_IP=$(kubectl get svc | grep nginx | awk '{print $4}')
+sed "s/FTPS_IP/$FTPS_IP/g" ./srcs/ftps/ftps.yaml.og > ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
