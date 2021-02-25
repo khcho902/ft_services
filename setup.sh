e0869b6af0feb4b180e08f16c86a3c39c2ab5b06
khcho902 <khcho902@gmail.com>
2021-02-25 21:59:22 +0900

    finish!!

--- setup.sh
+++ setup.sh
@@ -8,19 +8,35 @@ minikube addons enable metrics-server
 
 eval $(minikube docker-env)
 
-# Build docker images
-docker build -t nginx-image ./srcs/nginx
-docker build -t ftps-image ./srcs/ftps
-docker build -t mysql-image ./srcs/mysql
-docker build -t phpmyadmin-image ./srcs/phpmyadmin
-docker build -t wordpress-image ./srcs/wordpress
-
-# Apply yaml files
 kubectl apply -f ./srcs/metallb/metallb.yaml
-kubectl apply -f ./srcs/nginx/nginx.yaml
-export FTPS_IP=$(kubectl get svc | grep nginx | awk '{print $4}')
+
+
+# mysql 
+docker build -t mysql-image ./srcs/mysql
+kubectl apply -f ./srcs/mysql/mysql.yaml
+
+# phpmyadmin
+docker build -t phpmyadmin-image ./srcs/phpmyadmin
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+
+# wordpress
+docker build -t wordpress-image ./srcs/wordpress
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
+
+# ftps
+docker build -t ftps-image ./srcs/ftps
+export FTPS_IP=$(kubectl get svc | grep phpmyadmin | awk '{print $4}')
 sed "s/FTPS_IP/$FTPS_IP/g" ./srcs/ftps/ftps.yaml.og > ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/ftps/ftps.yaml
-kubectl apply -f ./srcs/mysql/mysql.yaml
-kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
-kubectl apply -f ./srcs/wordpress/wordpress.yaml
+
+# influxdb
+docker build -t influxdb-image ./srcs/influxdb
+kubectl apply -f ./srcs/influxdb/influxdb.yaml
+
+# grafana
+docker build -t grafana-image ./srcs/grafana
+kubectl apply -f ./srcs/grafana/grafana.yaml
+
+# nginx
+docker build -t nginx-image ./srcs/nginx
+kubectl apply -f ./srcs/nginx/nginx.yaml
